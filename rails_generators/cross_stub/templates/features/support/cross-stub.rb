24109021fb704181e05424717a5310feb6378c86
NgTzeYang <ngty77@gmail.com>
2010-07-16 16:39:37 +0800

    Added comment to make user aware of the available cache stores.

--- rails_generators/cross_stub/templates/features/support/cross-stub.rb
+++ rails_generators/cross_stub/templates/features/support/cross-stub.rb
@@ -1,3 +1,17 @@
 require 'cross-stub'
-Before { CrossStub.setup :file => File.join(RAILS_ROOT, 'tmp', 'cross-stub') }
-After  { CrossStub.clear }
+
+Before do
+  # /////////////////////////////////////////////////////////////////////////////////
+  # NOTE: By default, we use file-based cache store for cross-stub. Alternatively,
+  # u may wanna try out the other cache stores. Adding new cache store support is
+  # super easy, w.r.t actual implementation & testing, just drop me a note at
+  # http://github.com/ngty/cross-stub & i'll do it for u, of course FOC lah !!
+  # /////////////////////////////////////////////////////////////////////////////////
+  #CrossStub.setup :redis => 'localhost:6379/xstub.cache' # requires *redis* gem
+  #CrossStub.setup :memcache => 'localhost:11211/xstub.cache' # requires *memcache-client* gem
+  CrossStub.setup :file => File.join(RAILS_ROOT, 'tmp', 'xstub.cache')
+end
+
+After do
+  CrossStub.clear
+end
