dcbb04e7cd0d20b008667dc286d374dc3dbb5d2f
NgTzeYang <ngty77@gmail.com>
2010-10-15 16:42:22 +0800

    Use the latest Proc#to_sexp(:stip_enclosure => true, :attached_to => :xstub, &body_matcher) to better grab the associated code body.

--- lib/cross-stub/arguments/proc.rb
+++ lib/cross-stub/arguments/proc.rb
@@ -7,10 +7,16 @@ module CrossStub
 
         def parse(&block)
           methods = {}
-          block.to_sexp.each_of_type(:defn) do |_sexp|
+          proc_to_sexp(block).each_of_type(:defn) do |_sexp|
             methods[_sexp.to_a[1]] = RUBY_2_RUBY.process(_sexp)
           end
           methods
+        end
+
+        def proc_to_sexp(block)
+          block.to_sexp(:stip_enclosure => true, :attached_to => :xstub) do |body|
+            body =~ /^(.*\W|)def\W/
+          end
         end
 
       end
