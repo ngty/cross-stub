c47577f473cdda522232093ec57cd3c14c590a4a
NgTzeYang <ngty77@gmail.com>
2010-10-15 16:35:32 +0800

    Bacon now runs in a less verbose mode.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,22 @@ require 'rubygems'
 require 'bacon'
 require File.join(File.dirname(__FILE__), 'includes')
 
+Bacon.extend(Bacon::TestUnitOutput)
 Bacon.summary_on_exit
+
+if ENV['MUTE_BACON'] == 'true'
+  Bacon.extend(Module.new {
+
+    def handle_requirement(description)
+      unless (error = yield).empty?
+        print error[0..0]
+      end
+    end
+
+    def handle_summary
+      puts "", "  %d tests, %d assertions, %d failures, %d errors" %
+        Bacon::Counter.values_at(:specifications, :requirements, :failed, :errors)
+    end
+
+  })
+end
